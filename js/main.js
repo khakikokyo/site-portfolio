5336436338a7e065f4f41d6930fc4890ebdd6991
khakikokyo <phyeyoung90@gmail.com>
2022-11-12 03:10:00 +0900

    about me 타이틀/이미지/텍스트 효과

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// main_bg
+// about
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
