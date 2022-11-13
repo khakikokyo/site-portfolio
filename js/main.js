32c4e70d58d0e6b8eb81399792c99bedced7d2f4
khakikokyo <phyeyoung90@gmail.com>
2022-11-14 03:04:54 +0900

    skills 생성

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 // about
+// ScrollMagic
 const spyEls = document.querySelectorAll('section.scroll-spy');
 spyEls.forEach(function(spyEl) {
   new ScrollMagic
     .Scene({
       triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-      triggerHook: .8
+      triggerHook: .9
     })
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
+});
+
+// skills
+// Swiper
+new Swiper('.skills .swiper-container', {
+  autoplay: {
+    delay: 10000
+  },
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.skills .swiper-prev',
+    nextEl: '.skills .swiper-next'
+  },
 });
